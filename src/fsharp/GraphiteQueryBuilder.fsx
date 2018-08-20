f1663f60b829fafd121ca75af0f089e7b98a4f40
Luke Merrett <lukeamerrett@gmail.com>
2018-08-20 21:08:41 +0100

    Removed unnecessary whitespacing

--- src/fsharp/GraphiteQueryBuilder.fsx
+++ src/fsharp/GraphiteQueryBuilder.fsx
@@ -17,7 +17,7 @@ type Aggregation   = | Average | Median | Sum | Min | Max | Diff
  
 
 
-let private unionToString (x: 'a) =
+let private unionToString (x:'a) =
     match FSharpValue.GetUnionFields(x, typeof<'a>) with
     | case, _ -> case.Name
 
@@ -30,42 +30,42 @@ let absolute (query:Query): Query =
 
 let aggregate (aggregation:Aggregation) (query:Query) =
     let agg = aggregation |> unionToString |> lower 
-    sprintf "aggregate(%s, \"%s\")" query agg
+    sprintf "aggregate(%s,\"%s\")" query agg
 
 let aggregateLine (aggregation:Aggregation) (query:Query) =
     let agg = aggregation |> unionToString |> lower 
-    sprintf "aggregateLine(%s, \"%s\")" query agg
+    sprintf "aggregateLine(%s,\"%s\")" query agg
 
 let aggregateWithWildcards (aggregation:Aggregation) (query:Query) (position:Position) =
     let agg = aggregation |> unionToString |> lower 
-    sprintf "aggregateWithWildcards(%s, \"%s\", %i)" query agg position
+    sprintf "aggregateWithWildcards(%s,\"%s\",%i)" query agg position
 
 let alias (alias:Alias) (query:Query): Query = 
-    sprintf "alias(%s, \"%s\")" query alias
+    sprintf "alias(%s,\"%s\")" query alias
 
 let aliasByMetric (query:Query): Query = 
     sprintf "aliasByMetric(%s)" query
 
 let aliasByNode (node:Node) (query:Query): Query = 
-    sprintf "aliasByNode(%s, %i)" query node
+    sprintf "aliasByNode(%s,%i)" query node
 
 let aliasByTags (tag:Tag) (query:Query): Query = 
-    sprintf "aliasByTags(%s, \"%s\")" query tag
+    sprintf "aliasByTags(%s,\"%s\")" query tag
 
 let aliasQuery (search:SearchString) (replace:ReplaceString) (newName:Alias) (query:Query): Query = 
-    sprintf "aliasQuery(%s, \"%s\", \"%s\", \"%s\")" query search replace newName
+    sprintf "aliasQuery(%s,\"%s\",\"%s\",\"%s\")" query search replace newName
 
 let aliasSub (search:SearchString) (replace:ReplaceString) (query:Query): Query = 
-    sprintf "aliasSub(%s, \"%s\", \"%s\")" query search replace
+    sprintf "aliasSub(%s,\"%s\",\"%s\")" query search replace
 
 let alpha (query:Query) (alpha:Alpha): Query =
     if alpha < 0.0 || alpha > 1.0 then
         failwith "Alpha value must be between 0.0 and 1.0"
     else
-        sprintf "alpha(%s, %f)" query alpha            
+        sprintf "alpha(%s,%f)" query alpha            
 
 let applyByNode (node:Node) (templateQuery:Query) (query:Query): Query =
-    sprintf "applyByNode(%s, %i, \"%s\")" query node templateQuery
+    sprintf "applyByNode(%s,%i,\"%s\")" query node templateQuery
 
 let areaBetween(query:Query): Query =
     sprintf "areaBetween(%s)" query
@@ -75,19 +75,19 @@ let asPercent (query:Query): Query =
     // https://graphite.readthedocs.io/en/latest/functions.html#graphite.render.functions.asPercent
 
 let averageAbove (value:int) (query:Query): Query =
-    sprintf "averageAbove(%s, %i)" query value
+    sprintf "averageAbove(%s,%i)" query value
 
 let averageBelow (value:int) (query:Query): Query =
-    sprintf "averageBelow(%s, %i)" query value
+    sprintf "averageBelow(%s,%i)" query value
 
 let averageOutsidePercentile (value:int) (query:Query): Query =
-    sprintf "averageOutsidePercentile(%s, %i)" query value
+    sprintf "averageOutsidePercentile(%s,%i)" query value
 
 let averageSeries (query:Query): Query = 
      sprintf "averageSeries(%s)" query
 
 let averageSeriesWithWildcards (position:Position) (query:Query): Query = 
-     sprintf "averageSeriesWithWildcards(%s, %i)" query position
+     sprintf "averageSeriesWithWildcards(%s,%i)" query position
 
 
 // -----------------------------------------------------
@@ -100,7 +100,7 @@ let diffSeries (source:Query) (diffWith:Query): Query =
 
 let holtWintersConfidenceBands (delta:Delta) (query:Query): Query = 
     match delta with 
-    | Delta(d) -> sprintf "holtWintersConfidenceBands(%s, %i)" query d
+    | Delta(d) -> sprintf "holtWintersConfidenceBands(%s,%i)" query d
     
 let maxSeries (query:Query): Query = 
     sprintf "maxSeries(%s)" query       
@@ -109,13 +109,13 @@ let minSeries (query:Query): Query =
     sprintf "minSeries(%s)" query
 
 let removeBelowValue (value:int) (query:Query): Query =
-    sprintf "removeBelowValue(%s, %i)" query value
+    sprintf "removeBelowValue(%s,%i)" query value
 
 let summarise (summariseBy:SummariseBy) (query:Query): Query =
     match summariseBy with 
     | AverageBy(timeUnit) ->
         match timeUnit with
-        | Minutes(minutes) -> sprintf "summarize(%s, \"%iminutes\", \"avg\")" query minutes
+        | Minutes(minutes) -> sprintf "summarize(%s,\"%iminutes\",\"avg\")" query minutes
 
 let transformNull (value:int) (query:Query): Query =
-    sprintf "transformNull(%s, %i)" query value
+    sprintf "transformNull(%s,%i)" query value
